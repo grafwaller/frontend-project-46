f568deed63dc2250e74c4ed503f665909469474c
valeriy kukushkin <valeriy9791@yandex.ru>
2023-05-08 16:52:29 +0300

    update file

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,7 +8,5 @@ program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0')
-    .option('-V, --version', 'output the version number')
-    .option('-h, --help', 'display help for command');
-
+    
 program.parse();
